f6f090e6e119f1c23f3937d99b8fe715fe2c2e53
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2022-11-06 20:55:54 -0500

    practica 3

---
+++ practica2.py
@@ -0,0 +1,10 @@
+print("Inicio")
+
+texto = input("Ingrese: ")
+while texto != "salir":
+    print("eco:", texto)
+    texto = input("Ingrese: ")
+
+print("Fin")
+    
+    
---
+++ practica3.py
@@ -0,0 +1,7 @@
+num = input("Ingrese un número: ") 
+while not num.isdigit():
+    num = input("Ingrese un número: ")
+
+resultado = int(num)
+print("Fin")
+
