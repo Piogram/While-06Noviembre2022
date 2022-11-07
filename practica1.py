077c9974da563c4704692ec20b8d49d7a7046d34
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2022-11-06 20:41:56 -0500

    practica 1

---
+++ practica1.py
@@ -0,0 +1,5 @@
+print("Diga sí para continuar")
+respuesta = input("¿Desea continuar el programa? ").lower()
+while respuesta == "sí" :
+    respuesta = input("¿Desea continuar el programa? ").lower()
+print("¡Hasta la vista!")
