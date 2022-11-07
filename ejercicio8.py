5bfbe408dc2a7a8e7f05ea704b2fedcbdd35e791
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2022-11-06 22:52:58 -0500

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,4 +1,5 @@
-import random as rd
+import random as axell
+
 print("Lanzamientos de dados")
 cont=1
 dado = rd.randint(1, 6)
@@ -8,8 +9,8 @@ while dado != 6 and cont!=3:
     dado = rd.randint(1, 6)
     print('dado: ', dado)
 
-#print("Ocurrieron {} lanzamientos".format(cont))
 if dado==6:
+    print("Ocurrieron {} lanzamientos".format(cont))
     print("Ganaste")
 else:
     print("Perdiste")
